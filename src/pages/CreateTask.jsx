0ab8fb0ef738cd9f947b51287f014b3b4ba4f547
ammurana4216 <drona.ambeshwar@gmail.com>
2023-06-07 10:03:05 +0530

    '

---
+++ src/pages/CreateTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CreateTask(props) {
+    return (
+        <div>
+            Create task
+        </div>
+    );
+}
+
+export default CreateTask;
---
+++ src/pages/TaskList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TaskList(props) {
+    return (
+        <div>
+            Task list
+        </div>
+    );
+}
+
+export default TaskList;
